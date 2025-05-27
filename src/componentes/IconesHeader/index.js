eed4997194bfdf58c96b8817cb21c24fc1c5ba29
Gustavo Zesczylinski <germangz@rocketmail.com>
2025-05-27 15:30:19 -0300

    Introduction to react

---
+++ src/componentes/IconesHeader/index.js
@@ -0,0 +1,18 @@
+import './estilo.css';
+import perfil from '../../img/perfil.svg';
+import sacola from '../../img/sacola.svg';
+
+const icones = [perfil, sacola];
+
+function IconesHeader() {
+    return (
+        <ul className='icones'>
+          {icones.map((icone => 
+            <li className='icone'><img src={icone} alt='icone'></img></li>
+          ))
+          }
+        </ul>
+    )
+}
+
+export default IconesHeader;
